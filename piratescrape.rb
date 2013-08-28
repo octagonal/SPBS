985791b67894ba8565565e0a2a49abab0ae4bd4d
Anthony Madhvani <anny.mdvn@gmail.com>
2013-08-28 23:55:59 +0200

    Clean up code

--- piratescrape.rb
+++ piratescrape.rb
@@ -5,7 +5,6 @@ require 'open-uri'
 
 def fetch_magnet(url)
   url.gsub!(" ", "%20")
-  #puts "URL: #{url}"
 
   doc = Nokogiri::HTML(open("http://thepiratebay.sx/search/" + url + "/0/7/0"))
 
@@ -14,38 +13,6 @@ def fetch_magnet(url)
   size = ""
 
   torrent = doc.css("#searchResult > tr")
-
-  torrent.each do |t|
-
-    #puts "[#{t.css("td")[0].css("center a")[1].content}]\t#{t.css("td")[1].css("div.detName a").first.content}"
-    #puts "#{t.css("td")[2].inner_text}"
-    #(t.css("td")[2].inner_text.to_i/269).times {print "#"}
-    #puts "#{t.css("td")[1].css("> a")[0]["href"]}"
-
-    #puts t.css("td")[2].css(".detname a")["href"]
-
-    #puts "\n"
-
-=begin
-    t.css("td").each do |td|
-
-      td
-
-      if a["href"].to_s.start_with?("/torrent/")
-        #puts title
-        title = a.inner_text
-      end
-
-      if a["href"].to_s.start_with?("magnet")
-        magnet = a["href"]
-        #puts magnet
-        puts "\n"
-      end
-
-    end
-=end
-
-  end
 
   torrent.length.times do |current|
     puts "(#{current}):\t[#{torrent[current].css("td")[1].css("div.detName a").first.content}]?" #Naam
