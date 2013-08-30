38a39c9cd69d8b9348cb5d61343098eaaed11c41
Anthony Madhvani <anny.mdvn@gmail.com>
2013-08-31 00:15:34 +0200

    Removed interactivity

--- piratescrape.rb
+++ piratescrape.rb
@@ -2,8 +2,10 @@
 
 require 'nokogiri'
 require 'open-uri'
+require 'pp'
 
-def fetch_magnet(url)
+def fetch_magnet(query)
+  url = query.dup
   url.gsub!(" ", "%20")
 
   doc = Nokogiri::HTML(open("http://thepiratebay.sx/search/" + url + "/0/7/0"))
@@ -14,26 +16,19 @@ def fetch_magnet(url)
 
   torrent = doc.css("#searchResult > tr")
 
+  results = Hash.new
+
   torrent.length.times do |current|
-    puts "(#{current}):\t[#{torrent[current].css("td")[1].css("div.detName a").first.content}]?" #Naam
-    puts "\t#{torrent[current].css("td")[2].inner_text} Seeders\n\t#{torrent[current].css("td")[0].css("center a")[1].content}\n\n" #seeders en categorie
-    puts "#{torrent[current].css("td")[1].css("> a")[0]["href"]}" #magnet link
-    puts "Downloaden? (y,n,q)"
-    answer = gets.chomp
-    if    answer == "y" or answer == "yes"
-      command = torrent[current].css('td')[1].css('> a')[0]['href']
-      command = "open '#{command}'"
-      exec command
-    elsif answer == "n" or answer == "no"
-      next
-    elsif answer == "q" or answer == "quit"
-      break
-    else
-      next
-    end
+    puts current
+    #puts "(#{current}):\t[#{torrent[current].css("td")[1].css("div.detName a").first.content}]?" #Naam
+    #puts "\t#{torrent[current].css("td")[2].inner_text} Seeders\n\t#{torrent[current].css("td")[0].css("center a")[1].content}\n\n" #seeders en categorie
+    #puts "#{torrent[current].css("td")[1].css("> a")[0]["href"]}" #magnet link
+    #puts "Downloaden? (y,n,q)"
+    #answer = gets.chomp
+    #command = torrent[current].css('td')[1].css('> a')[0]['href']
   end
 
 end
 
-puts "Wat wil je downloaden?"
-fetch_magnet(gets.chomp)
+#puts "Wat wil je downloaden?"
+fetch_magnet(ARGV[0])
