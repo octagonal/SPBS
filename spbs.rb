e1e5ecfbe70582c4885a5231c2cdea3d6c2b2e50
Anthony Madhvani <anny.mdvn@gmail.com>
2013-09-04 15:44:10 +0200

    Added result class

--- spbs.rb
+++ spbs.rb
@@ -1,6 +1,10 @@
+require 'json'
 require_relative 'spbs/fetch.rb'
 require_relative 'spbs/query.rb'
 
 query = SPBS::Query.new(
-  :query => "Oreimo S01 E30"
+  :query => "The Newsroom 2012 S02E07 720p HDTV x264 EVOLVE"
 )
+
+request = SPBS::Fetch.new(query.queryset)
+request.resultset.each {|test| puts test.url}
